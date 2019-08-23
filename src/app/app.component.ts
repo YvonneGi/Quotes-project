60e00ce7932dc828268cedde1235e1a212038d3e
YvonneGi <giryvonne@gmail.com>
2019-08-23 15:57:19 +0200

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  // title = 'Quotes';
+  // quote = 'Belive in youself'
+  // quotes:string[];
+
+  // constructor(){
+  //   this.quotes = ['The only way to do great things is to love what you do',
+  //    'Dont worry about rejection all you need is one person to say yes', 'Try to be rainbow in someones cloud']
+  // } 
+  quotes:Quote[] = [
+    {id:1, name:'The only way to do great things is to love what you do'},
+    {id:2,name:'Dont worry about rejection all you need is one person to say yes'},
+    {id:3,name:'Try to be rainbow in someones cloud'},
+    // {id:4,name:'Get Dog Food'},
+    // {id:5,name:'Solve math homework'},
+    // {id:6,name:'Plot my world domination plan'},
+  ];
 }
